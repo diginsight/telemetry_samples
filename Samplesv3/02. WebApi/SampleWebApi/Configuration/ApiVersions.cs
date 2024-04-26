2ad95671c8a5bafbde00d0073200a3ed3c5ee2f0
DARIO AIROLDI <dario.airoldi@it.abb.com>
2024-04-26 11:53:50 -0700

    # Use Dynamic-Configuration to manage configurations and feature flags dynamically at runtime

---
+++ Samplesv3/02. WebApi/SampleWebApi/Configuration/ApiVersions.cs
@@ -0,0 +1,13 @@
+﻿using Asp.Versioning;
+using Diginsight.CAOptions;
+
+namespace SampleWebApi;
+
+public static class ApiVersions
+{
+    public static class V_2024_04_26
+    {
+        public const string Name = "2024-04-26";
+        public static ApiVersion Version = new ApiVersion(new DateOnly(2024, 04, 26));
+    }
+}
