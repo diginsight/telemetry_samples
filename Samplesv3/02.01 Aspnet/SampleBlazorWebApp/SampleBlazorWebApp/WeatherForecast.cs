6c86e441f9c6ed67e0c99c6821b305a096cd0099
Dario Airoldi <darioa@microsoft.com>
2024-06-28 06:24:57 -0700

    sample SampleBlazorWebApp

---
+++ Samplesv3/02.01 Aspnet/SampleBlazorWebApp/SampleBlazorWebApp/WeatherForecast.cs
@@ -0,0 +1,19 @@
+using Diginsight.Strings;
+
+namespace SampleBlazorWebApp
+{
+    public class WeatherForecast
+    {
+        [LogStringableMember(Order = 1)]
+        public DateOnly Date { get; set; }
+
+        [LogStringableMember(Order = 2)]
+        public int TemperatureC { get; set; }
+
+        [NonLogStringableMember]
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        [LogStringableMember(Order = 3)]
+        public string? Summary { get; set; }
+    }
+}
