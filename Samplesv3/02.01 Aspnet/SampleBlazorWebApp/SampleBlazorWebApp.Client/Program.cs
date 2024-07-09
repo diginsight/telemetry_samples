b14e21676fa9b35f132493d5e9559f8a6b61ef47
Dario Airoldi <darioa@microsoft.com>
2024-07-09 16:50:09 +0200

    blazor fix

--- Samplesv3/02.01 Aspnet/SampleBlazorWebApp/SampleBlazorWebApp.Client/Program.cs
+++ Samplesv3/02.01 Aspnet/SampleBlazorWebApp/SampleBlazorWebApp.Client/Program.cs
@@ -1,5 +1,30 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Diginsight.Diagnostics;
+using Diginsight;
+using SampleBlazorWebApp.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-await builder.Build().RunAsync();
+var services = builder.Services;
+
+services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    //loggingBuilder.AddConsole();
+    loggingBuilder.AddDiginsightConsole();
+});
+
+services.Configure<DiginsightActivitiesOptions>(options =>
+{
+    options.LogActivities = true;
+    //options.ActivitySources.Add(Observability.ActivitySource.Name);
+});
+
+
+builder.ConfigureContainer(new DiginsightServiceProviderFactory(new ServiceProviderOptions()));
+
+var host = builder.Build();
+
+
+await host.RunAsync();
+
