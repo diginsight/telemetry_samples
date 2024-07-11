88dbcb370653d4bbd4a0ddf81976a923a3d2310d
Dario Airoldi <darioa@microsoft.com>
2024-07-11 18:59:07 +0200

    auth sample with telemetry

--- Samplesv3/02.01 Aspnet/SampleBlazorAuthenticatedApp/SampleBlazorAuthenticatedApp.Client/Program.cs
+++ Samplesv3/02.01 Aspnet/SampleBlazorAuthenticatedApp/SampleBlazorAuthenticatedApp.Client/Program.cs
@@ -1,11 +1,34 @@
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Diginsight.Diagnostics;
+using Diginsight;
 using SampleBlazorAuthenticatedApp.Client;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var services = builder.Services;
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
-await builder.Build().RunAsync();
+//services.AddLogging(loggingBuilder =>
+//{
+//    //loggingBuilder.ClearProviders();
+//    //loggingBuilder.AddConsole();
+//    //loggingBuilder.AddDiginsightConsole();
+//});
+
+//services.Configure<DiginsightActivitiesOptions>(options =>
+//{
+//    options.LogActivities = true;
+//    //options.ActivitySources.Add(Observability.ActivitySource.Name);
+//});
+
+
+builder.ConfigureContainer(new DiginsightServiceProviderFactory(new ServiceProviderOptions()));
+
+var host = builder.Build();
+
+
+await host.RunAsync();
+
