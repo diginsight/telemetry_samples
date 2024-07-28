18d15c6c036dff40c8749494872fb3dbf1904d9f
Dario Airoldi <darioa@microsoft.com>
2024-07-28 20:43:08 +0200

    configuration

--- Samplesv3/02.02 Functions/Basic/SampleStartup.cs
+++ Samplesv3/02.02 Functions/Basic/SampleStartup.cs
@@ -1,8 +1,7 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
+﻿using System.IO;
 using System.IO;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,23 +11,49 @@ namespace Microsoft.Azure.Functions.Samples.DependencyInjectionBasic
 {
     public class SampleStartup : FunctionsStartup
     {
+        //public static ILoggerFactory LoggerFactory;
+        //private readonly ILogger logger;
+
+        //private static readonly JsonSerializerOptions MyJsonSerializerOptions = new(JsonSerializerOptions.Default) { ReadCommentHandling = JsonCommentHandling.Skip };
+        //public static IHost host;
+        //private readonly IConfiguration configuration;
+        //private readonly IFileProvider fileProvider;
+
         public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
         {
+            //var activitiesOptions = new DiginsightActivitiesOptions() { LogActivities = true };
+            //var deferredLoggerFactory = new DeferredLoggerFactory(activitiesOptions: activitiesOptions);
+            //deferredLoggerFactory.ActivitySources.Add(Observability.ActivitySource);
+            //LoggerFactory = deferredLoggerFactory;
+            //var logger = LoggerFactory.CreateLogger<Program>();
+
+            //using var activity = Observability.ActivitySource.StartMethodActivity(logger, new { args });
+
             FunctionsHostBuilderContext context = builder.GetContext();
 
             // Note that these files are not automatically copied on build or publish. 
             // See the csproj file to for the correct setup.
             builder.ConfigurationBuilder
                 .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
-                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: false);
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables()
+                .AddUserSecrets<SampleStartup>();
+                
+
         }
 
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddScoped<IGreeter, SampleGreeter>();
 
+
+            builder.Services.AddScoped<IGreeter, SampleGreeter>();
             FunctionsHostBuilderContext context = builder.GetContext();
+            
+            var configuration = context.Configuration;
+
+
             builder.Services.Configure<SampleOptions>(context.Configuration);
         }
+
     }
 }
